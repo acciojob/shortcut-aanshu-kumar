8ac7de8a0214fd57ee2e378ef6480913e12a3373
Anshu kumar <40786793+aanshu-kumar@users.noreply.github.com>
2024-12-13 13:00:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function shortcut(s1, s2) {
   // your code here
+	let res;
+	if(s1.length==0 || s2.length==0)
+		res='';
+	else
+		res=s1[0]+s2[0];
+	return res;
+	
 }
+
 
 // Do not change the code below.
 const s1 = prompt("Enter s1:");
